e5b20cdf3dcdedaf16c06129fa3f2e70a4c25352
Gustav <gustavmangion@hotmail.com>
2023-05-27 16:15:10 +0400

    Add init function

--- calculate.js
+++ calculate.js
@@ -1,6 +1,14 @@
+let screenText = "zzz ...";
+const screen = document.querySelector(".screen");
+
 function buttonPressed(e) {
 	console.log("moew");
 }
 
-const buttons = document.querySelectorAll(".key");
-buttons.forEach((x) => x.addEventListener("click", buttonPressed));
+function init() {
+	const buttons = document.querySelectorAll(".key");
+	buttons.forEach((x) => x.addEventListener("click", buttonPressed));
+	screen.innerText = screenText;
+}
+
+init();

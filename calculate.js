bc16e1b1060242ae8df47e3e3b87dd661834bf15
Gustav <gustavmangion@hotmail.com>
2023-05-27 16:35:45 +0400

    Change button font sizr

--- calculate.js
+++ calculate.js
@@ -2,7 +2,26 @@ let screenText = "zzz ...";
 const screen = document.querySelector(".screen");
 
 function buttonPressed(e) {
-	console.log("moew");
+	console.log(e);
+	if (e.id !== "") doFunctionKey(e.target.id);
+	else doKeyPressed(e.target);
+	screen.innerText = screenText;
+}
+
+function doFunctionKey(key) {
+	console.log(key);
+}
+
+function getKeyType(key) {
+	console.log(key.id);
+}
+
+function doKeyPressed(e) {
+	const action = e.target.innerText;
+	console.log(action);
+	// switch (action) {
+	//     case
+	// }
 }
 
 function init() {
